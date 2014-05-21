ed8ea8de065cea8a24c8a9b14a07b8c159a1fda2
SZR <szymon.rut@agileo.it>
2014-05-21 10:06:45 +0200

    Created Address model

---
+++ app/models/address.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+class Address < ActiveRecord::Base
+  belongs_to :company
+
+  validates :company, presence: true
+
+  def to_s
+     (first_part + comma + second_part).split.join(" ").gsub(" ,", ",")
+  end
+
+  private
+  def first_part
+    "#{street} #{building}#{"/#{unit}" if !unit.empty?}"
+  end
+
+  def second_part
+    "#{zip_code} #{city}"
+  end
+
+  def comma
+    (!street.empty? || !building.empty? || !unit.empty?) && (!zip_code.empty? || !city.empty?) ? ", " : ""
+  end
+end
