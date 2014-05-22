bc9dc9b36bae15640a9ec5ee247e5742a1c48651
SZR <szymon.rut@agileo.it>
2014-05-22 10:56:08 +0200

    Added couple helpers in Application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def active_class(model)
+    request.path.split("/")[2].to_i.eql?(model.id) ? "active" : ""
+  end
+
+  def disabled_link(text)
+    link_to text, "javascript:;", class: "disabled_link"
+  end
+
+  def edit_link(model)
+    show? ? link_to("Edytuj dane", [:edit, model]) : disabled_link("Edytuj dane")
+  end
+
+  def show?
+    action_name.eql?("show")
+  end
 end
