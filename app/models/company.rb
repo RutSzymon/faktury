acb57b3752c13829f45ffb89feaf4c2ef39c287a
SZR <szymon.rut@agileo.it>
2014-05-22 10:56:51 +0200

    Added person and phone validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,8 @@ class Company < ActiveRecord::Base
   has_one :address, dependent: :destroy
 
   validates :name, presence: true
+  validates :person, presence: true
+  validates :phone, presence: true
 
   accepts_nested_attributes_for :address
 
