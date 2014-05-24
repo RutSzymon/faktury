ffa1817db12fb71fe8f8958e2542171224929c16
SZR <szymon.rut@agileo.it>
2014-05-24 23:49:07 +0200

    Added link_to_add_fields helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,15 @@ module ApplicationHelper
     show? ? link_to("Edytuj dane", [:edit, model]) : disabled_link("Edytuj dane")
   end
 
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    link_to(name, "javascript:;", class: "add_fields btn", data: { id: id, fields: fields.gsub("\n", "") })
+  end
+
   def show?
     action_name.eql?("show")
   end
