494f34a1d9f597ec33cedbb890fc00db76528c96
SZR <szymon.rut@agileo.it>
2014-05-24 23:49:59 +0200

    In Invoice: 1) Added orders and products associations 2) Accepted nested attributes for oders 3) Added payment_date, sum and tax_sum methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,14 +3,30 @@ class Invoice < ActiveRecord::Base
   PAYMENT_TIME = [7, 14, 21, 30]
 
   belongs_to :contractor
+  has_many :orders
+  has_many :products, through: :orders
 
   validates :contractor, presence: true
   validates :date, presence: true
   validates :payment_time, presence: true, inclusion: { in: PAYMENT_TIME }
   validate :check_date
 
+  accepts_nested_attributes_for :orders, allow_destroy: true
+
   def to_s
     "Faktura z dnia #{date}"
+  end
+
+  def payment_date
+    date + payment_time.days
+  end
+
+  def sum
+    orders.map(&:amount).sum
+  end
+
+  def tax_sum
+    orders.map(&:tax_amount).sum
   end
 
   private
