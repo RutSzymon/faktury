2beabdace7007c754b8a10633b5e5a78eccc8df3
SZR <szymon.rut@agileo.it>
2014-05-24 23:50:51 +0200

    In Product: 1) Added other scope 2) Added gross_price and tax_rate methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,8 @@ class Product < ActiveRecord::Base
   validates :price, presence: true
   validates :tax, presence: true, inclusion: { in: TAX }
 
+  scope :other, -> (products) { where.not(id: products) }
+
   def to_s
     name
   end
@@ -16,5 +18,13 @@ class Product < ActiveRecord::Base
 
   def price
     price_in_cents.to_d/100 if price_in_cents
+  end
+
+  def gross_price
+    price + tax_rate
+  end
+
+  def tax_rate
+    price*tax.to_d/100
   end
 end
