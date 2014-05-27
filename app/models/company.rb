6bceab42611beafaf8dfceb3de6d13dabb308132
SZR <szymon.rut@agileo.it>
2014-05-27 15:05:44 +0200

    In Company: 1) Included NipMethods concern 2) Improved phone validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,12 @@
 # encoding: utf-8
 class Company < ActiveRecord::Base
+  include NipMethods
+
   has_one :address, dependent: :destroy
 
   validates :name, presence: true
   validates :person, presence: true
-  validates :phone, presence: true
+  validates :phone, numericality: true, format: { with: /^(\d{9}|\d{11})$/, multiline: true }
 
   accepts_nested_attributes_for :address
 
